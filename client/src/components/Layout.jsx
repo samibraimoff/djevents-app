243605b62c5888f49d8face6bb5091df4bc5630d
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2022-12-03 01:09:40 -0500

    add showcase and styles with images

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import Header from "./Header";
 import Footer from "./Footer";
+import Showcase from "./Showcase";
 import styles from "../../styles/Layout.module.css";
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -14,6 +17,7 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="keywords" content={keywords} />
       </Head>
       <Header />
+      {router.pathname === "/" && <Showcase />}
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
